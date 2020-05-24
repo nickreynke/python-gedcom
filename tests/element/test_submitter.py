6bafc97dee958b3a1b50f9cf47fa846c89704d60
Christopher Horn <cdhorn@embarqmail.com>
2020-05-24 00:32:19 -0400

    fix: fix typo in SubmitterElement test

--- tests/element/test_submitter.py
+++ tests/element/test_submitter.py
@@ -6,4 +6,4 @@ import gedcom.tags
 def test_initialization():
     submitter_element = SubmitterElement(level=-1, pointer="", tag=gedcom.tags.GEDCOM_TAG_SUBMITTER, value="")
     assert isinstance(submitter_element, Element)
-    assert isinstance(submitter_element, SourceElement)
+    assert isinstance(submitter_element, SubmitterElement)
