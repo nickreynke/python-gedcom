0ce24ac8e746d3d565313e853ae071328c418c8a
Nicklas Reincke <nickreynke@gmail.com>
2020-08-08 07:57:02 +0200

    docs: simplified copyright texts

--- tests/element/test_note.py
+++ tests/element/test_note.py
@@ -1,3 +1,14 @@
+#  Copyright (C) 2020
+#
+#  This file is part of the Python GEDCOM Parser.
+#
+#  You should have received a copy of the GNU General Public License along
+#  with this program; if not, write to the Free Software Foundation, Inc.,
+#  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+#  For more, have a look at the GitHub repository at:
+#  https://github.com/nickreynke/python-gedcom
+
 from gedcom.element.element import Element
 from gedcom.element.note import NoteElement
 import gedcom.tags
